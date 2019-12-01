336734f2cd760b97746bf4622df763d7770cfe7f
maajor <hello_myd@126.com>
2019-12-01 15:02:52 +0800

    Upgrade to Unity 2019, Fix bug "Thread group count is above the maximum allowed limit. Maximum allowed thread group count is 65535"

--- NNPostProcessing/Assets/NNPostProcessing/Scripts/NNLayers/Tanh.cs
+++ NNPostProcessing/Assets/NNPostProcessing/Scripts/NNLayers/Tanh.cs
@@ -32,9 +32,17 @@ namespace NNPP
 
         public override void Run(object[] input)
         {
+            //Unity2019 refuse threadgroup > 65536, so we make a 2d input threadgroup
+            int threadGroupX = Mathf.CeilToInt(OutputShape.y * OutputShape.z / 32.0f);
+            NNCompute.Instance.Shader.SetInts("InputShapeIdMultiplier", new int[3]
+            {
+                OutputShape.x,
+                1,
+                1
+            });
             NNCompute.Instance.Shader.SetBuffer(KernelId, "LayerInput0", input[0] as ComputeBuffer);
             NNCompute.Instance.Shader.SetBuffer(KernelId, "LayerOutput", outputbuffer);
-            NNCompute.Instance.Shader.Dispatch(KernelId, Mathf.CeilToInt(OutputShape.x * OutputShape.y * OutputShape.z / 32.0f), 1, 1);
+            NNCompute.Instance.Shader.Dispatch(KernelId, threadGroupX, OutputShape.x, 1);
         }
     }
 }
